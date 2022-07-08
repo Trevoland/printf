701cc6d81a4c508197f531be6c53c99a175506da
trevoland nagoor <trevolandn26@gmail.com>
2022-07-08 13:14:56 -0700

    task

--- main.h
+++ main.h
@@ -33,9 +33,9 @@ typedef struct printHandler
 int _printf(const char *format, ...);
 
 /* print_nums */
-int print_int(va_list 1, flags_t *f);
+int print_int(va_list l, flags_t *f);
 void print_number(int n);
-int print_unsigned(va_list 1, flags_t *f);
+int print_unsigned(va_list l, flags_t *f);
 int count_digit(int i);
 
 /* print_bases */
@@ -43,3 +43,10 @@ int print_hex(va_list l, flags_t *f);
 int print_hex_big(va_list l, flags_t *f);
 int print_binary(va_list l, flags_t *f);
 int print_octal(va_list l, flags_t *f);
+
+/* converter */
+char *convert(unsigned long int num, int base, int lowercase);
+
+/* get_print */
+int (*get_print(char s))(va_list, flags_t *);
+
