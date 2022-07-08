480469a24c57e574553c5e70678f6b1672bc813f
trevoland nagoor <trevolandn26@gmail.com>
2022-07-08 12:27:39 -0700

    task

--- main.h
+++ main.h
@@ -1,5 +1,41 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed _printf()
+ * @plus: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+type struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: formate specifier
+ * @f: pointer to the correct printing function
+ */
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+
+/* _printf */
 int _printf(const char *format, ...);
-int (*get_print(char s))(va_list,flags_t *);
+
+/* print_nums */
+int print_int(va_list 1, flags_t *f);
+void print_number(int n);
+int print_unsigned(va_list 1, flags_t *f);
+int count_digit(int i);
+
+#endif
