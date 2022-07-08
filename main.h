b66b1f32c73e6a31b69fd0ee8d36994dc0fee181
trevoland nagoor <trevolandn26@gmail.com>
2022-07-08 13:09:03 -0700

    task

--- main.h
+++ main.h
@@ -38,4 +38,8 @@ void print_number(int n);
 int print_unsigned(va_list 1, flags_t *f);
 int count_digit(int i);
 
-#endif
+/* print_bases */
+int print_hex(va_list 1, flags_t *f);
+int print_hex_big(va_list 1, flags_t *f);
+int print_binary(va_list 1, flags_t *f);
+int print_octal(va_list 1, flags_t *f);
