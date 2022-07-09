4a722f706eafb506fc9763f9c6517b9e3fac4e1f
Kummirai <ajaxmilton@hotmail.com>
2022-07-09 11:39:35 -0700

    Reference for get_flag

---
+++ get_flag.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_flag - turns on flag if _printf finds a flag in the string
+ * @s: character that hold flag specifier
+ * @f: pointer the struct flags
+ * Return: 1 if flag is truned on or 0 if otherwise
+ */
+int get_flag(char s, flag_t *f)
+{
+	int n;
+
+	n = 0;
+
+	switch (s)
+	{
+		case'#':
+			f->hash = 1;
+			break;
+
+		case'+':
+			f->plus = 1;
+			break;
+
+		case' ':
+			f->space = 1;
+			break;
+	}
+	return (n);
+}
