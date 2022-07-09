8823469853456d1508800bbae1c22a7e7f409719
Trevoland <104766934+Trevoland@users.noreply.github.com>
2022-07-09 22:14:10 +0200

    Update main.c

--- tests/main.c
+++ tests/main.c
@@ -1,44 +1,38 @@
+#include "../main.h"
 #include <limits.h>
 #include <stdio.h>
-#include "main.h"
 
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
+
 int main(void)
 {
-    int len;
-    int len2;
-    unsigned int ui;
-    void *addr;
+	char a = 'a';
+	char *b = NULL;
+	long int c = INT_MAX, l = UINT_MAX + 1024;
+	int count0, count1;
+	unsigned int ui = (unsigned int)INT_MAX + 1024;
 
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)0x7ffe637541f0;
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
-    _printf("Unsigned:[%u]\n", ui);
-    printf("Unsigned:[%u]\n", ui);
-    _printf("Unsigned octal:[%o]\n", ui);
-    printf("Unsigned octal:[%o]\n", ui);
-    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
-    _printf("Address:[%p]\n", addr);
-    printf("Address:[%p]\n", addr);
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
-    _printf("Len:[%d]\n", len);
-    printf("Len:[%d]\n", len2);
-    _printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n");
-    return (0);
+	count0 = _printf("simple statement\n");
+	count1 = _printf("simple statement\n");
+	printf("count [%d, %d]\n", count0, count1);
+
+	count0 = _printf("%c\n", a);
+	count1 = printf("%c\n", a);
+	printf("count [%d, %d]\n", count0, count1);
+
+	count0 = _printf("%s\n", b);
+	count1 = printf("%s\n", b);
+	printf("count [%d, %d]\n", count0, count1);
+
+	count0 = _printf("%d\n", c);
+	count1 = printf("%d\n", c);
+	printf("count [%d, %d]\n", count0, count1);
+
+	count0 = _printf("%%\n");
+	count1 = printf("%%\n");
+	printf("count [%d, %d]\n", count0, count1);
+	_printf("%b\n", UINT_MAX);
+	_printf("%b\n", l);
+	_printf("Unsigned: [%u]\n", ui);
+	_printf("Unsigned octal:[%o]\n", ui);
+	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
 }
