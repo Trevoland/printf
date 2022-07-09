34ba390210067175d935627f60610a56b7b618d0
Trevoland <104766934+Trevoland@users.noreply.github.com>
2022-07-09 12:10:08 +0200

    Update main.h

--- main.h
+++ main.h
@@ -6,32 +6,29 @@
 
 /**
  * struct flags - struct containing flags to "turn on"
- * when a flag specifier is passed _printf()
+ * when a flag specifier is passed to _printf()
  * @plus: flag for the '+' character
- * @space: flag for the ' ' charater
+ * @space: flag for the ' ' character
  * @hash: flag for the '#' character
  */
-type struct flags
+typedef struct flags
 {
-	int plus;
-	int space;
-	int hash;
+    int plus;
+    int space;
+    int hash;
 } flags_t;
 
 /**
  * struct printHandler - struct to choose the right function depending
  * on the format specifier passed to _printf()
- * @c: formate specifier
+ * @c: format specifier
  * @f: pointer to the correct printing function
  */
 typedef struct printHandler
 {
-	char c;
-	int (*f)(va_list ap, flags_t *f);
+    char c;
+    int (*f)(va_list ap, flags_t *f);
 } ph;
-
-/* _printf */
-int _printf(const char *format, ...);
 
 /* print_nums */
 int print_int(va_list l, flags_t *f);
@@ -47,6 +44,9 @@ int print_octal(va_list l, flags_t *f);
 
 /* converter */
 char *convert(unsigned long int num, int base, int lowercase);
+
+/* _printf */
+int _printf(const char *format, ...);
 
 /* get_print */
 int (*get_print(char s))(va_list, flags_t *);
