2cabe3e38d4045c313a3ec4a4d8c40f6fdcb666a
Trevoland <104766934+Trevoland@users.noreply.github.com>
2022-07-10 19:53:06 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -15,6 +15,11 @@ int (*find_function(const char *format))(va_list)
 		{"s", print_string},
 		{"i", print_int},
 		{"d", print_dec},
+		{"b", print_bin},
+		{"u", print_unsig},
+		{"o", print_octal},
+		{"x", print_x},
+		{"X", print_X},
 		{NULL, NULL}
 	};
 
