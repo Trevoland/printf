56aa6f436d3174294eecd5a45c6cfd6e234e192d
Kummirai <ajaxmilton@hotmail.com>
2022-07-10 16:12:23 +0200

    Remove print_rev.c

--- main.h
+++ main.h
@@ -5,28 +5,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * struct flags - struct containing flags to "turn on"
- * when a flag specifier is passed to _printf()
- * @plus: flag for the '+' character
- * @space: flag for the ' ' character
- * @hash: flag for the '#' character
- */
-typedef struct flags
-{
-        int plus;
-        int space;
-        int hash;
-} flags_t;
-
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(va_list c);
 int print_string(va_list s);
 int print_int(va_list i);
 int print_dec(va_list d);
-int print_rev(va_list l, flags_t *f);
-
 /**
   * struct code_format - Struct format
   * @sc: The specifiers
