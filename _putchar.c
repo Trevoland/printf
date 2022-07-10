61d51b66f87b5b3d87bf157dfe106b44d407ddd9
Kummirai <ajaxmilton@hotmail.com>
2022-07-10 13:22:25 +0200

    Fix code

--- _putchar.c
+++ _putchar.c
@@ -10,32 +10,5 @@
 
 int _putchar(char c)
 {
-	static char buff[1024];
-	static int i;
-
-	if (c == -1 || i <= 1024)
-	{
-		write(1, &buff, 1);
-		i = 0;
-	}
-	if ( c != -1)
-	{
-		buff[i] = c;
-		i++;
-	}
-	return (1);
-}
-
-/**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
- * Return: number of chars written
- */
-int _puts(char *str)
-{
-	register int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-		_putchar(str[i]);
-	return (i);
+	return write(1, &c, 1);
 }
