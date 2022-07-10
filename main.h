beb5a945a90a3b21d69ab2ab27e60ce55592bdd6
Trevoland <104766934+Trevoland@users.noreply.github.com>
2022-07-10 19:51:56 +0200

    Update main.h

--- main.h
+++ main.h
@@ -11,6 +11,12 @@ int print_char(va_list c);
 int print_string(va_list s);
 int print_int(va_list i);
 int print_dec(va_list d);
+int print_bin(va_list b);
+int print_unsig(va_list u);
+int print_octal(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+
 /**
   * struct code_format - Struct format
   * @sc: The specifiers
